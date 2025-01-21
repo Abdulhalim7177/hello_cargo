5663b7879b01a8cf98fcd4e10659b237bef72abf
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-01-21 14:08:28 +0100

    add input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
 fn main() {
-    println!("this is rust app");
+
+    let mut input = String::new();
+    print!("please enter a number ");
+
+    io::stdin().read_line(&mut input)
+    .expect("failed to read line");
+    let number: i32 = input.trim().parse()
+    .expect("please enter a valid number");
+
+    println!("you entered the number {number}");
 }
